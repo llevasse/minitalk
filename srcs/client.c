3f0eb2dda3b47847de7a6daf2d407528fc3e59e8
Leo <leonardlevasseur@yahoo.com>
2023-06-07 22:01:11 +0200

    stable I guess and I should not play with revert

--- srcs/client.c
+++ srcs/client.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 22:41:46 by llevasse          #+#    #+#             */
-/*   Updated: 2023/05/31 22:18:08 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/06/07 22:00:24 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,28 +29,12 @@ int	main(int argc, char **argv)
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-	if (argc == 4)
-		if (!use_file(pid, argv))
-			return (1);
 	if (argc == 3)
-		send_str(pid, argv[2]);
-	send_char(pid, '\n');
-	send_char(pid, '\0');
+		send_str(pid, (unsigned char *)argv[2]);
+	send_char(pid, (unsigned char)'\n');
+	send_char(pid, (unsigned char)'\0');
 	while (1)
 		;
-	return (1);
-}
-
-int	use_file(int pid, char **argv)
-{
-	if (ft_strcmp(argv[2], "-t") || !ft_strnstr(argv[3], ".txt",
-			ft_strlen(argv[3])))
-	{
-		ft_printf("Error. Invalid argument\n");
-		ft_printf("sould be : ./client {pid} -t {file.txt} \n");
-		return (0);
-	}
-	send_file(pid, open(argv[3], O_RDONLY));
 	return (1);
 }
 

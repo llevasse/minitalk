fb5c3174d73adc00c30e587ec15d7edc2f117838
Leo <leonardlevasseur@yahoo.com>
2023-06-07 23:41:08 +0200

    use read instead of gnl

--- srcs/extra/ft_send_extra.c
+++ srcs/extra/ft_send_extra.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 15:26:40 by llevasse          #+#    #+#             */
-/*   Updated: 2023/06/07 12:58:07 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/06/07 23:38:27 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,61 @@
 
 void	send_file(int pid, int fd, t_extra extra)
 {
-	unsigned char	*str;
+	unsigned char	str[42];
+	int				count;
 
-	str = get_next_line(fd);
-	if (!str)
-		return ((void)(close(fd), ft_exit("Unable to open file", 1)));
+	//str = get_next_line(fd);
+	count = read(fd, str, 42);
+	if (count < 0 || str[0] == 0)
+		return ((void)(close(fd),
+						ft_exit("Unable to open file",
+								1)));
 	if (extra.binnary_logged == 1)
 		write(extra.log_fd, "{", 1);
-	send_str(pid, str, extra);
-	while (str)
+	send_str_file(pid, str, extra);
+	while (count > 0)
 	{
-		free(str);
-		str = NULL;
-		str = get_next_line(fd);
-		if (!str)
+		count = read(fd, str, 42);
+		if (str[0] == 0)
 			break ;
-		send_str(pid, str, extra);
+		send_str_file(pid, str, extra);
 	}
 	send_char(pid, (unsigned char)'\n', extra);
 	send_char(pid, (unsigned char)'\0', extra);
 	close(fd);
 }
 
-void	send_str(int pid, unsigned char *str, t_extra extra)
+void	send_str_file(int pid, unsigned char str[42], t_extra extra)
 {
+	int	i;
+
+	i = 0;
 	if (!extra.from_txt && extra.binnary_logged)
 		write(extra.log_fd, "{", 1);
-	while (*str)
+	while (str[0])
 	{
-		if (!send_char(pid, *str, extra))
-			exit((int)(free(str), 0));
-		str++;
+		if (!send_char(pid, str[0], extra))
+			exit(0);
+		i++;
 	}
 	if (!extra.from_txt)
 	{
 		send_char(pid, (unsigned char)'\n', extra);
 		send_char(pid, (unsigned char)'\0', extra);
-	}	
+	}
+}
+void	send_str(int pid, unsigned char *str, t_extra extra)
+{
+	if (extra.binnary_logged)
+		write(extra.log_fd, "{", 1);
+	while (*str)
+	{
+		if (!send_char(pid, str[0], extra))
+			exit(0);
+		str++;
+	}
+	send_char(pid, (unsigned char)'\n', extra);
+	send_char(pid, (unsigned char)'\0', extra);
 }
 
 int	send_char(int pid, unsigned char c, t_extra extra)

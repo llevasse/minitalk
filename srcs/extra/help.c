005286c1fbd883dbbc22f36f6c1d9e7ffa67043d
Leo <leonardlevasseur@yahoo.com>
2023-06-11 10:19:18 +0200

    added helper for ./server

--- srcs/extra/help.c
+++ srcs/extra/help.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 19:13:00 by llevasse          #+#    #+#             */
-/*   Updated: 2023/06/11 10:03:39 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/06/11 10:18:52 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	ft_taste_the_rainbow(char *str)
 		print_color(&rgb);
 		ft_printf("%c", *(str++));
 	}
+	ft_printf("\x1B[1;m");
 }
 
 void	print_help_server(void)
@@ -29,9 +30,9 @@ void	print_help_server(void)
 	ft_printf("\n%-10sPrint this helper. \
 				\n%-10sPrint character when fully received, \
 don't wait for whole str. \
-				\n%-10sPrint read characters in client_log.log. \
+				\n%-10sPrint read characters in server_log.log. \
 				\n%-10sPrint read characters (in binary and ASCII) \
-in client_log.log.\
+in server_log.log.\
 				\n%-10s",
 				"-h",
 				"-c",

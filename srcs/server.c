cef4b6b57a122b3cf32909e84e238b7462341d64
Leo <leonardlevasseur@yahoo.com>
2023-06-13 17:09:26 +0200

    normed mandatory server.c

--- srcs/server.c
+++ srcs/server.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 13:22:34 by llevasse          #+#    #+#             */
-/*   Updated: 2023/06/13 14:55:23 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/06/13 17:06:38 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,14 @@ void	sig_handler(int sig, siginfo_t *siginfo, void *context)
 	if (sig == SIGUSR2)
 		g_sig_char.c |= (1 << g_sig_char.shift);
 	g_sig_char.shift--;
+	print_sig_char(siginfo);
+	if (kill(g_sig_char.client_pid, SIGUSR1) == -1)
+		ft_exit("Error in sending signal", 0);
+	(void)context;
+}
+
+void	print_sig_char(siginfo_t *siginfo)
+{
 	if (g_sig_char.shift < 0)
 	{
 		if (!g_sig_char.mini_str)
@@ -39,9 +47,6 @@ void	sig_handler(int sig, siginfo_t *siginfo, void *context)
 		g_sig_char.shift = 7;
 		g_sig_char.c = 0;
 	}
-	if (kill(g_sig_char.client_pid, SIGUSR1) == -1)
-		ft_exit("Error in sending signal", 0);
-	(void)context;
 }
 
 void	ft_exit(char *str, int status)

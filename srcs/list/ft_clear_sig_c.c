8f997a1244814ed2b6d8116da53697de3ed0bb2d
Leo <leonardlevasseur@yahoo.com>
2023-06-14 11:13:10 +0200

    multiple definition of free_last

--- srcs/list/ft_clear_sig_c.c
+++ srcs/list/ft_clear_sig_c.c
@@ -6,13 +6,13 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 10:56:13 by llevasse          #+#    #+#             */
-/*   Updated: 2023/06/14 10:58:45 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/06/14 11:12:17 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minitalk_extra.h"
 
-void	free_last(t_sig_char *lst);
+void	free_last_sig_c(t_sig_char *lst);
 
 void	ft_clear_sig_c(t_sig_char **lst)
 {
@@ -24,14 +24,14 @@ void	ft_clear_sig_c(t_sig_char **lst)
 		while ((*lst)->next != NULL)
 			*lst = (*lst)->next;
 		while (temp->next != NULL)
-			free_last(temp);
+			free_last_sig_c(temp);
 		free(temp);
 	}
 	*lst = NULL;
 	return ;
 }
 
-void	free_last(t_sig_char *lst)
+void	free_last_sig_c(t_sig_char *lst)
 {
 	t_sig_char	*temp;
 
